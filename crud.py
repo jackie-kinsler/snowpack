9738e3e32acdb781a4cc1f06a55d81449d4d8a25
Jackie Kinsler <jackiekinsler@gmail.com>
2020-06-02 16:13:53 -0700

    pulled trail data from HikingProject API and added to trails.json. Seeded trail data in seed_database.py

--- crud.py
+++ crud.py
@@ -4,15 +4,29 @@ from model import db, User, Movie, Rating, connect_to_db
 from datetime import datetime 
 
 
-def create_user(email, password):
-    """Create and return a new user."""
+# def create_user(email, password):
+#     """Create and return a new user."""
 
-    user = User(email = email, password = password)
+#     user = User(email = email, password = password)
 
-    db.session.add(user)
-    db.session.commit()
+#     db.session.add(user)
+#     db.session.commit()
 
-    return user
+#     return user
+
+# def create_movie(title = None, overview = None, release_date = None, poster_path = None):
+#     """Create and return a new movie"""
+
+#     movie = Movie(title = title, 
+#                   overview = overview, 
+#                   release_date = release_date, 
+#                   poster_path = poster_path)
+
+#     db.session.add(movie)
+#     db.session.commit()
+
+#     return movie
+
 
 
 if __name__ == '__main__':
--- seed_database.py
+++ seed_database.py
@@ -9,33 +9,42 @@ import crud
 import model
 import server
 
-os.system('dropdb ratings')
-os.system('createdb ratings')
+os.system('dropdb mapping')
+os.system('createdb mapping')
 
 model.connect_to_db(server.app)
 model.db.create_all()
 
-with open('data/movies.json') as f:
-    movie_data = json.loads(f.read())
+with open('data/trails.json') as f:
+    trail_data = json.loads(f.read())
 
 
-# Create movies, store them in list so we can use them
-# to create fake ratings later
-movies_in_db = []
-for movie in movie_data:
-    title = movie.get('title')
-    overview = movie.get('overview')
-    poster_path = movie.get('poster_path')
-    release_date = datetime.strptime(movie.get('release_date'), '%Y-%m-%d')
+# Create trails, store them in list
 
-    movie = crud.create_movie(title, overview, release_date, poster_path)
-
-    movies_in_db.append(movie)
- 
-for n in range(10):
-    email = f'user{n}@test.com'
-    password = 'test'
-    user = crud.create_user(email, password)
+trails_in_db = []
+for trail in trail_data:
+    name = trail.get('name')
+    desc = trail.get('summary')
+    long = trail.get('longitude')
+    lat = trail.get('latitude')
+    kml = None 
+    length = trail.get('length')
+    ascent = trail.get('ascent')
+    descent = trail.get('descent')
+    difficulty = trail.get('difficulty')
+    location = trail.get('location')
+    url = trail.get('url')
+    img = trail.get('imgMedium')
     
-    for _ in range(10):
-        crud.create_rating(user, choice(movies_in_db), randint(1,5))
+    
+    trail = crud.create_movie(title, overview, release_date, poster_path)
+
+    trails_in_db.append(trail)
+ 
+# for n in range(10):
+#     email = f'user{n}@test.com'
+#     password = 'test'
+#     user = crud.create_user(email, password)
+    
+#     for _ in range(10):
+#         crud.create_rating(user, choice(movies_in_db), randint(1,5))
