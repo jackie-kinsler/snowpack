eb40d58c9f00e3d5703558ec85d4246f0a535e06
Jackie Kinsler <jackiekinsler@gmail.com>
2020-06-04 14:05:28 -0700

    Added bootstrap datepicker plugin to select date for snowform

---
+++ static/js/base.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    var date_input=$('input[name="date"]'); //our date input has the name "date"
+    var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+    date_input.datepicker({
+        format: 'mm/dd/yyyy',
+        container: container,
+        todayHighlight: true,
+        autoclose: true,
+    })
+})
