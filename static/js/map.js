8d3cb825691c6cbaad60b1fafff0d32960fd9022
Jackie Kinsler <jackiekinsler@gmail.com>
2020-06-11 18:45:36 -0700

    Added searchbox to map

--- static/js/map.js
+++ static/js/map.js
@@ -45,11 +45,54 @@ function calendarMap(day, month, year, url, thLat, thLong, initial_zoom = 7, cen
             NOAAOverlay.setMap(map);
     };
 
-    // google.maps.event.addListener(map, 'bounds_changed', () => {
-    //     window.setTimeout( () => {
-    //         console.log('change!')
-    //     }, 3000);
-    // });
+    var input = document.getElementById("search-box");
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+
+
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+          return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function(marker) {
+          marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+          if (!place.geometry) {
+            console.log("Returned place contains no geometry");
+            return;
+          }
+
+          // Create a marker for each place.
+          markers.push(new google.maps.Marker({
+            map: map,
+            title: place.name,
+            position: place.geometry.location
+          }));
+
+          if (place.geometry.viewport) {
+            // Only geocodes have viewport.
+            bounds.union(place.geometry.viewport);
+          } else {
+            bounds.extend(place.geometry.location);
+          }
+        });
+        map.fitBounds(bounds);
+      });
 }
 
 
