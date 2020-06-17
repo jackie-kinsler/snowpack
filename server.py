b3273b8bad034a159cdc914dc839c150cd9f3e6d
Jackie Kinsler <jackiekinsler@gmail.com>
2020-06-17 13:56:13 -0700

    No functional changes - cleaning code.

--- server.py
+++ server.py
@@ -7,43 +7,105 @@ from jinja2 import StrictUndefined
 from datetime import datetime 
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = 'static/GPS/user_uploads'
-ALLOWED_EXTENSIONS = {'kml','json','geojson','application/json','js'}
 
 app = Flask(__name__)
 app.secret_key = 'dev'
 app.jinja_env.undefined = StrictUndefined
+
+UPLOAD_FOLDER = 'static/GPS/user_uploads'
+ALLOWED_EXTENSIONS = {'kml','json','geojson','application/json','js'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # limit file upload size to 20MB
 app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024
 
+##########################
+# This file contains three sections: 
+# 1. Routes that render pages
+# 2. API routes
+# 3. Helper functions 
+##########################
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
+##########################
+# ROUTES THAT RENDER PAGES 
+##########################
 
 @app.route('/')
 def homepage():
-    """Render root page of website"""
+    """Render root of website"""
     
-    return render_template('homepage.html', today = datetime.date(datetime.now()))
+    return render_template('homepage.html', 
+                           today = datetime.date(datetime.now()))
 
 
-@app.route('/log-in')
+@app.route('/trails')
+def trail_page():
+    """Render page with map and trail filtering capability."""
+
+    return render_template('trailpage.html')
+
+
+@app.route('/favorite-trails')
+def favorite_trail():
+    """Render page of user's favorite trails (only if logged in)."""
+
+    user_id = session.get('user_id')
+    if user_id:
+        favorite_trails = crud.get_favorites_by_user_id(user_id)
+        return render_template('favorite-trails.html', 
+                               favorite_trails = favorite_trails)
+    else: 
+        flash('Log In to see favorite trails')
+        return redirect('/')
+
+
+@app.route('/add-a-trail', methods=['GET', 'POST'])
+def add_a_trail():
+    """Render page where users can add a suggested trail (only if logged in)."""
+    
+    if request.method == 'GET':
+        user_id = session.get('user_id')
+        if user_id:
+            return render_template("add_a_trail.html")
+        else: 
+            flash('Log In to add a trail')
+            return redirect('/')
+    
+    if request.method == 'POST':
+        create_suggestion_from_user_inputs()
+        
+        flash('Your trail has been added to suggestions!' +
+              ' Give us some time - you\'ll see it on the trailpage soon! :)')
+        return redirect('/add-a-trail')
+
+
+@app.route('/moderator')
+def moderator_page():
+    """Render moderator page. Here, suggestions can be added, edited, or deleted."""
+    suggestions = crud.get_all_suggested()
+
+    return render_template('moderator.html', suggestions = suggestions)
+
+
+##########################
+# API ROUTES 
+##########################
+
+
+@app.route('/api/log-in')
 def log_in():
+    """Check user input email/password against User table in db"""
+
     email = request.args.get('email')
     password = request.args.get('password')
 
     user = crud.get_user_by_email(email)
-    print('**********')
-    print(user)
 
+    # if credentials are in db, the user_id and moderator boolean
+    # are added to session 
     if user.password == password: 
         flash('Logged In!')
         session['user_id'] = user.user_id
         session['moderator'] = user.moderator
-        print(session)
 
         return redirect('/')
     else:
@@ -52,18 +114,17 @@ def log_in():
     
 @app.route('/log-out')
 def log_out():
+    """Log out user and clear the session."""
 
     session.clear()
-
-    print(session)
 
     flash("Logged out.")
 
     return "Logged out."
 
-@app.route('/users', methods = ['POST'])
+@app.route('/api/create-user', methods = ['POST'])
 def create_account():
-    """Create a new user"""
+    """Create a new user."""
 
     email = request.form.get('email')
     password = request.form.get('password')
@@ -78,14 +139,7 @@ def create_account():
     return redirect('/')
 
 
-@app.route('/trails')
-def trail_page():
-    """Show a trail page that has a list of trails and a map"""
-
-
-    return render_template('trailpage.html')
-
-@app.route('/filtered-trails')
+@app.route('/api/filtered-trails')
 def filtered_trail(): 
     """Return a list of trails filtered on distance"""
     
@@ -93,15 +147,12 @@ def filtered_trail():
     min_dist = request.args.get('min_dist')
     max_dist = request.args.get('max_dist')
 
-    if trail_name == '' and min_dist == '' and max_dist == '':
-        return("Please enter at least one filtering parameter")
-
     if min_dist == '':
         min_dist = 0
     if max_dist == '': 
         max_dist = 99999
     
-    if trail_name == None: 
+    if trail_name == '': 
         trails = crud.trails_by_distance(min_dist, max_dist)
     else:
         trails = crud.trails_by_name_distance(min_dist, max_dist, trail_name)
@@ -145,80 +196,6 @@ def add_to_favorites():
 
     return (trail.name + " added to favorites!")
 
-@app.route('/favorite-trails')
-def favorite_trail():
-    """Page to display a user's favorite trails"""
-
-    user_id = session.get('user_id')
-    if user_id:
-        favorite_trails = crud.get_favorites_by_user_id(user_id)
-        return render_template('favorite-trails.html', favorite_trails = favorite_trails)
-    else: 
-        flash('Log In to see favorite trails')
-        return redirect('/')
-
-
-@app.route('/add-a-trail', methods=['GET', 'POST'])
-def add_a_trail():
-    """Users can suggest a trail to be added to the site."""
-    
-    if request.method == 'GET':
-        user_id = session.get('user_id')
-        print(session)
-        if user_id:
-            return render_template("add_a_trail.html")
-        else: 
-            flash('Log In to add a trail')
-            return redirect('/')
-    
-    if request.method == 'POST':
-        name = request.form.get('name')
-        desc = request.form.get('description')
-        long = float(request.form.get('long'))
-        lat = float(request.form.get('lat'))
-        length = float(request.form.get('length'))
-        length_unit = request.form.get('length-unit')
-        if length_unit == 'kilometers':
-            length *= 0.621371
-        ascent = request.form.get('ascent')
-        ascent_unit = request.form.get('ascent-unit')
-        if ascent_unit == 'meters':
-            ascent *= 3.28084
-        descent = request.form.get('descent')
-        descent_unit = request.form.get('descent-unit')
-        if descent_unit == 'meters':
-            descent *= 3.28084
-        difficulty = request.form.get('difficulty')
-        location = request.form.get('location')
-        url = request.form.get('url')
-        print("*****")
-        print(url)
-        gps = ""
-        user_id = session.get('user_id')
-        user = crud.get_user_by_id(user_id)
-
-        file = request.files.get('file')
-
-
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            # gps is the name of the file path to the gps information
-            # used for building the Suggestion database
-            gps = (os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-        crud.create_suggested_trail(name, desc, lat, long, gps, length, 
-                                    ascent, descent, difficulty, location, url, user)
-        flash('Your trail has been added to suggestions! Give us some time - you\'ll see it on the trailpage soon! :)')
-        return redirect('/add-a-trail')
-
-@app.route('/moderator')
-def moderator_page():
-    """Moderators can manage the Suggestion database."""
-    suggestions = crud.get_all_suggested()
-
-    return render_template('moderator.html', suggestions = suggestions)
 
 @app.route('/moderator/add-to-trail-db', methods = ['POST'])
 def add_suggestion_to_trail_db():
@@ -227,9 +204,6 @@ def add_suggestion_to_trail_db():
     suggestion_id = request.form.get('suggestion_id')
 
     suggestion = crud.get_suggestion_by_id(suggestion_id)
-    print("****")
-    print(suggestion)
-    print(suggestion_id)
     
     crud.create_trail(suggestion.name, 
                       suggestion.desc, 
@@ -245,7 +219,6 @@ def add_suggestion_to_trail_db():
                       None
                     )
     
-    print(suggestion.name)
     return("Suggestion added to trail db")
 
 @app.route('/moderator/delete-suggestion', methods = ['POST'])
@@ -253,7 +226,6 @@ def delete_suggestion():
     """Delete a bad suggestion."""
     
     suggestion_id = request.form.get('suggestion_id')
-    print(suggestion_id)
 
     crud.delete_suggestion_by_id(suggestion_id)
 
@@ -263,8 +235,6 @@ def delete_suggestion():
 def edit_suggestion(suggestion_id):
 
     suggestion = crud.get_suggestion_by_id(suggestion_id)
-    print(suggestion)
-
 
     return render_template('edit_suggestion.html', suggestion = suggestion)
 
@@ -306,9 +276,6 @@ def store_map_details():
         session['center_lat'] = center_lat
         session['center_long'] = center_long
 
-        print(zoom, center_lat, center_long)
-        print("****")
-        print(session)
         return ('success')
     
     if request.method == 'GET':
@@ -316,8 +283,59 @@ def store_map_details():
                       'center_lat' : session.get('center_lat'), 
                       'center_long' : session.get('center_long')
                      }
-        print(map_params)
         return jsonify(map_params)
+
+
+##########################
+# HELPER FUNCTIONS 
+##########################
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def create_suggestion_from_user_inputs():
+    """Parses the form from /add-a-trail"""
+    name = request.form.get('name')
+    desc = request.form.get('description')
+    long = float(request.form.get('long'))
+    lat = float(request.form.get('lat'))
+    length = float(request.form.get('length'))
+    length_unit = request.form.get('length-unit')
+    if length_unit == 'kilometers':
+        length *= 0.621371
+    ascent = request.form.get('ascent')
+    ascent_unit = request.form.get('ascent-unit')
+    if ascent_unit == 'meters':
+        ascent *= 3.28084
+    descent = request.form.get('descent')
+    descent_unit = request.form.get('descent-unit')
+    if descent_unit == 'meters':
+        descent *= 3.28084
+    difficulty = request.form.get('difficulty')
+    location = request.form.get('location')
+    url = request.form.get('url')
+    print("*****")
+    print(url)
+    gps = ""
+    user_id = session.get('user_id')
+    user = crud.get_user_by_id(user_id)
+    file = request.files.get('file')
+
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        # gps is the name of the file path to the gps information
+        # used for building the Suggestion database
+        gps = (os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    crud.create_suggested_trail(name, desc, lat, long, gps, length, 
+                                ascent, descent, difficulty, location, url, user)
+    return 
+
+
 
 if __name__ == '__main__':
     connect_to_db(app)
--- static/js/trailpage.js
+++ static/js/trailpage.js
@@ -5,94 +5,100 @@ console.log('trailpage.js has been loaded')
 $( document ).ready(function() {
     
     $("#distance-form").on('submit', (evt) => {
-        
+
+        // evt.stopImmediatePropagation();
         evt.preventDefault();
 
+        const trailName = $('#trail-name').val();
+        const minDist = $('#min-dist').val();
+        const maxDist = $('#max-dist').val();
+
+        if (!trailName && !minDist && !maxDist){
+            alert("Please enter at least one filtering parameter");
+            return;
+        }
+
         formInputs = {
-            'trail_name' : $('#trail-name').val(),
-            'min_dist' : $('#min-dist').val(),
-            'max_dist' : $('#max-dist').val()
+            'trail_name' : trailName,
+            'min_dist' : minDist,
+            'max_dist' : maxDist,
         };        
         
-        $.get('/filtered-trails', formInputs, (res) => {
+        $.get('/api/filtered-trails', formInputs, (res) => {
             
-            if (typeof res === 'object') {
-                if (res.length !== 0) {
-                    $("#filtered-trails").empty();
-                    $("#filtered-trails").text("Trails filtered by distance:");
-                    $("#trail-table").empty();
-                    $("#trail-table").append(`
-                        <tr>
-                            <th>Trail Name</th>
-                            <th>Length</th>
-                            <th>Location</th>
-                            <th>Favorite Trail</th>
-                            <th>Show On Map (only available for certain trails)</th>
-                        </tr>`
-                    );
-                    
-                    for (var trail of res) {
-                        if (trail['trail_gps']) {
-                            $("#trail-table").append(`
-                                <tr>
-                                    <td><a href=${trail['trail_url']}>${trail['trail_name']}</a></td>
-                                    <td>${trail['trail_distance']}</td>
-                                    <td><a href=https://www.google.com/maps/dir/Current+Location/${trail['trail_lat']},${trail['trail_long']}>${trail['trail_location']}</a></td>
-                                    <td><button id=${trail['trail_id']} class="favorite-button">Add Trail to Favorites</button></td>
-                                    <td><button id=${trail['trail_gps']} class="display-button" title="${trail['trail_lat']}:${trail['trail_long']}">Display GPS on map</button></td>
-                                </tr>`
-                            );
-                        } else {
-                            $("#trail-table").append(`
-                                <tr>
-                                    <td><a href=${trail['trail_url']}>${trail['trail_name']}</a></td>
-                                    <td>${trail['trail_distance']}</td>
-                                    <td><a href=https://www.google.com/maps/dir/Current+Location/${trail['trail_lat']},${trail['trail_long']}>${trail['trail_location']}</a></td>
-                                    <td><button id=${trail['trail_id']} class="favorite-button">Add Trail to Favorites</button></td>
-                                    <td><button class="display-button" title="${trail['trail_lat']}:${trail['trail_long']}">Display trailhead on map</button></td>
-                                </tr>`
-                            );
-                        }
+            if (res.length) {
+                $("#filtered-trails").empty();
+                $("#filtered-trails").text("Trails filtered by distance:");
+                $("#trail-table").empty();
+                $("#trail-table").append(`
+                    <tr>
+                        <th>Trail Name</th>
+                        <th>Length</th>
+                        <th>Location</th>
+                        <th>Favorite Trail</th>
+                        <th>Show On Map (only available for certain trails)</th>
+                    </tr>`
+                );
+                
+                for (var trail of res) {
+                    if (trail['trail_gps']) {
+                        $("#trail-table").append(`
+                            <tr>
+                                <td><a href=${trail['trail_url']}>${trail['trail_name']}</a></td>
+                                <td>${trail['trail_distance']}</td>
+                                <td><a href=https://www.google.com/maps/dir/Current+Location/${trail['trail_lat']},${trail['trail_long']}>${trail['trail_location']}</a></td>
+                                <td><button id=${trail['trail_id']} class="favorite-button">Add Trail to Favorites</button></td>
+                                <td><button id=${trail['trail_gps']} class="display-button" title="${trail['trail_lat']}:${trail['trail_long']}">Display GPS on map</button></td>
+                            </tr>`
+                        );
+                    } else {
+                        $("#trail-table").append(`
+                            <tr>
+                                <td><a href=${trail['trail_url']}>${trail['trail_name']}</a></td>
+                                <td>${trail['trail_distance']}</td>
+                                <td><a href=https://www.google.com/maps/dir/Current+Location/${trail['trail_lat']},${trail['trail_long']}>${trail['trail_location']}</a></td>
+                                <td><button id=${trail['trail_id']} class="favorite-button">Add Trail to Favorites</button></td>
+                                <td><button class="display-button" title="${trail['trail_lat']}:${trail['trail_long']}">Display trailhead on map</button></td>
+                            </tr>`
+                        );
                     }
-
-                    $('.favorite-button').on('click', () => {
-                        console.log('favorite-button clicked');
-
-                        var trailId = $(event.target).attr("id");
-                        
-                        formInputs = {
-                            'trail_id' : trailId,
-                        }
-                        
-                        $.get('/add-to-favorites', formInputs, (res) => {
-                            alert(res);
-                        });
-                    });
-
-                    $('.display-button').on('click', () => {
-                        console.log('you clicked?');
-
-                        var url = $(event.target).attr("id");
-
-                        // this gets the latLong of the trailhead, then splits it
-                        // into an array of the format latLong = [latitude, longitude]
-                        var latLong = $(event.target).attr("title").split(":");
-
-                        var today = new Date();
-                        var day = String(today.getDate()).padStart(2, '0');
-                        var month = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-                        var year = today.getFullYear();
-                        
-                        var initial_zoom = 10;
-
-                        trailMap(day, month, year, url, Number(latLong[0]), Number(latLong[1]), initial_zoom, Number(latLong[0]), Number(latLong[1]));
-
-                    });
-                } else {
-                    alert("No trails found with that filtering criteria.");
                 }
-            } else { 
-                alert(res); 
+
+                $('.favorite-button').on('click', () => {
+                    console.log('favorite-button clicked');
+
+                    var trailId = $(event.target).attr("id");
+                    
+                    formInputs = {
+                        'trail_id' : trailId,
+                    }
+                    
+                    $.get('/add-to-favorites', formInputs, (res) => {
+                        alert(res);
+                    });
+                });
+
+                $('.display-button').on('click', () => {
+                    console.log('you clicked?');
+
+                    var url = $(event.target).attr("id");
+
+                    // this gets the latLong of the trailhead, then splits it
+                    // into an array of the format latLong = [latitude, longitude]
+                    var latLong = $(event.target).attr("title").split(":");
+
+                    var today = new Date();
+                    var day = String(today.getDate()).padStart(2, '0');
+                    var month = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+                    var year = today.getFullYear();
+                    
+                    var initial_zoom = 10;
+
+                    trailMap(day, month, year, url, Number(latLong[0]), Number(latLong[1]), initial_zoom, Number(latLong[0]), Number(latLong[1]));
+
+                });
+            } else {
+                alert("No trails found with that filtering criteria.");
             }
         });
     }); 
