edc7a117fa69a1d4e5eede982f24a0c372f2fbc2
Jackie Kinsler <jackiekinsler@gmail.com>
2020-07-15 12:28:53 -0700

    Turned off toggling of background.

--- static/js/about.js
+++ static/js/about.js
@@ -1,14 +1,14 @@
 
-console.log("about.js loaded");
+// console.log("about.js loaded");
 
-$( document ).ready(function() {
-    $("#show-background").hover(function() {
-        $("body").css('background-image', 'url("/static/images/shuksan.jpg")');
-        $("html").css('background-image', 'url("/static/images/shuksan.jpg")');
-        $("#hide").hide();
-    }, function() {
-        $("body").css('background-image', 'linear-gradient(rgb(255, 255, 255), rgba(255, 255, 255, 0.65)), url("/static/images/shuksan.jpg")');
-        $("html").css('background-image', 'linear-gradient(rgb(255, 255, 255), rgba(255, 255, 255, 0.65)), url("/static/images/shuksan.jpg")');
-        $("#hide").show();
-    });
-}); 
+// $( document ).ready(function() {
+//     $("#show-background").hover(function() {
+//         $("body").css('background-image', 'url("/static/images/shuksan.jpg")');
+//         $("html").css('background-image', 'url("/static/images/shuksan.jpg")');
+//         $("#hide").hide();
+//     }, function() {
+//         $("body").css('background-image', 'linear-gradient(rgb(255, 255, 255), rgba(255, 255, 255, 0.65)), url("/static/images/shuksan.jpg")');
+//         $("html").css('background-image', 'linear-gradient(rgb(255, 255, 255), rgba(255, 255, 255, 0.65)), url("/static/images/shuksan.jpg")');
+//         $("#hide").show();
+//     });
+// }); 
